dd399212ca0399ae96cb9cb9a7dded497377d70a
Masterflash <sxq@yandex.ru>
2019-08-16 08:42:07 +0300

    start

---
+++ config/module.config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+*
+*/
+
+namespace Mf\GallerySimple;
+
+return [
+];
---
+++ src/Module.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* 
+*/
+
+namespace Mf\GallerySimple;
+
+
+class Module
+{
+
+public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+
+
+}
